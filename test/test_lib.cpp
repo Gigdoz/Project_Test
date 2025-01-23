4712ca0045d4c4c790d736bee996af44834e0aa0
Dudin Danil <danil.dudin@vis-technology.ru>
2025-01-24 00:44:17 +0500

    Добавлен исходник для тестов

--- test/test_lib.cpp
+++ test/test_lib.cpp
@@ -1,22 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
 #include "../lib/math.h"
-#include <iostream>
+#include "catch.h"
 
-void test_1() {
-    const char* flag = Sum(2, 3) == 2 + 3 ? "Passed" : "Failed";
-    std::cout << "1. Test Sum int: " << flag << "\n";
+TEST_CASE("Sum int") {
+    CHECK(Sum(2, 3) == 2 + 3);
 }
 
-void test_2() {
-    const char* flag = Sum(2.323, 3.12) == 2.323 + 3.12 ? "Passed" : "Failed";
-    std::cout << "2. Test Sum double: " << flag << "\n";
-}
-
-
-int main() {
-    std::cout << "\n\n-----------------Start testing-------------------\n\n";
-    std::cout << "  math.cpp\n";
-    test_1();
-    test_2();
-    std::cout << "\n\n-----------------Finish testing-------------------\n\n";
-    return 0;
+TEST_CASE("Sum double") {
+    CHECK(Sum(2.323, 3.12) == 2.323 + 3.12);
 }
